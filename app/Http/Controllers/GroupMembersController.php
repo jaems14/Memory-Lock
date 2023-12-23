c566fb96a864806d491039fe6b3263e9da6bbbce
james <james.santiago@dict.gov.ph>
2023-12-23 19:25:57 +0800

    update

---
+++ app/Http/Controllers/GroupMembersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Group_Members;
+use Illuminate\Http\Request;
+
+class GroupMembersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Group_Members  $group_Members
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Group_Members $group_Members)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Group_Members  $group_Members
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Group_Members $group_Members)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Group_Members  $group_Members
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Group_Members $group_Members)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Group_Members  $group_Members
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Group_Members $group_Members)
+    {
+        //
+    }
+}
